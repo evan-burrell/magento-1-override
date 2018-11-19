9742fd2a4f92b90a97c90a474d2106a5fa4fa074
Evan Burrell <evan.burrell@ampersandit.co.uk>
2018-11-19 11:53:52 +0000

    Config value

--- app/code/community/EB/Override/Model/Catalog/Product.php
+++ app/code/community/EB/Override/Model/Catalog/Product.php
@@ -4,7 +4,10 @@ class EB_Override_Model_Catalog_Product extends Mage_Catalog_Model_Product
 {
     protected function _afterLoad()
     {
-        $this->setName("Hijacked!!! " . $this->getName());
+        $config = Mage::getStoreConfig('eb_settings/module/override');
+        if ($config) {
+            $this->setName("Hijacked!!! " . $this->getName());
+        }
         return parent::_afterLoad();
     }
 }
